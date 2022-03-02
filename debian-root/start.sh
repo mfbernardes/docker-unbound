9ac6f38978c93174f98c800be2b4b8f390295991
Murillo Bernardes <mfbernardes@gmail.com>
2022-03-02 23:22:56 +0100

    Generate configuration from env vars

--- debian-root/start.sh
+++ debian-root/start.sh
@@ -1,9 +1,36 @@
 #!/bin/sh
 
+UNBOUND_CONF=/etc/unbound/unbound.conf
 
-echo "Preparing unbound..."
+VERBOSITY=${VERBOSITY:-0}
+
+LISTEN_IPV4=${LISTEN_IPV4:-127.0.0.1}
+LISTEN_IPV6=${LISTEN_IPV6:-::1}
+LISTEN_PORT=${LISTEN_PORT:-5553}
+
+
+ALLOW_SUBNETS=${ALLOW_SUBNETS:-""}
+
+DISABLE_IPV4=${DISABLE_IPV4:-yes}
+ENABLE_IPV6=${ENABLE_IPV6:-no}
+
+SO_RCVBUF=${SO_RCVBUF:-0}
+SO_REUSEPORT=${SO_REUSEPORT:-no}
+
+
+echo "Generating configuration..."
+
+eval "echo \"$(cat /etc/unbound/unbound.conf.tmpl)\"" > ${UNBOUND_CONF}
+
+set -- $ALLOW_SUBNETS
+while [ -n "$1" ]; do
+  echo "    access-control: $1 allow"
+  shift
+done >> ${UNBOUND_CONF}
+
+
+echo "Setting up keys..."
 /usr/lib/unbound/package-helper root_trust_anchor_update
 
 echo "Starting unbound"
-/usr/sbin/unbound -d -c /etc/unbound/unbound.conf
-
+/usr/sbin/unbound -d -c ${UNBOUND_CONF}
